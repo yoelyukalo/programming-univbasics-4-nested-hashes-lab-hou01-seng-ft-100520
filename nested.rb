4aabfbf462e12b35fdbc405541b6c3581dcabcc4
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-18 20:17:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -123,7 +123,7 @@ def adding_to_dennis
       },
       :dennis_ritchie => {
         :known_for => "Unix",
-        :languages => ["C" "Assembly"]
+        :languages => ["C", "Assembly"]
       }
     }
 
